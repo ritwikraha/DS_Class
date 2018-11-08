24ba969c8645b672e31ef780ff8cb55b393765f1
Aritra Roy Gosthipaty <aritra.born2fly@gmail.com>
2018-11-08 17:39:57 +0530

    structures basics!!

--- essentials/structures.c
+++ essentials/structures.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+void main(){
+	
+	//creating a structure,look at the elements
+	struct record{
+		char * name;
+		char * address;
+		int age;
+	}aritra={"aritra","81-Milan park,Kolkata,700084",20};
+	//two structures created namely aritra and momo with valuse initialised
+	struct record momo={"Moniseeta","81-Milan park,Kolkata,700084",27};
+	//structure elements are accessed with . operators
+	printf("%s\n",momo.name);
+	printf("%s\n",aritra.address);
+	printf("%d\n",momo.age);
+}
