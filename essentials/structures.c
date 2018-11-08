1ef97e003197dab85e414d11da54f3d8c4b0d4f7
Aritra Roy Gosthipaty <aritra.born2fly@gmail.com>
2018-11-08 17:52:26 +0530

    struct with pointers

--- essentials/structures.c
+++ essentials/structures.c
@@ -14,4 +14,16 @@ void main(){
 	printf("%s\n",momo.name);
 	printf("%s\n",aritra.address);
 	printf("%d\n",momo.age);
+	
+	
+	
+	//using a pointer in the structure
+	struct record * ptr= (struct record *)malloc(sizeof(struct record));
+	ptr->name="Gautam";
+	ptr->address="81-Miln Park";
+	ptr->age=60;
+	
+	printf("%s\n",ptr->name);
+	printf("%s\n",ptr->address);
+	printf("%d\n",ptr->age);
 }
