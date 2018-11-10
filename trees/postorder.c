74049148f5bd65153b20c408a5e65ef592246427
Aritra Roy Gosthipaty <aritra.born2fly@gmail.com>
2018-11-10 08:38:28 +0530

    pre in and post order of trees

---
+++ trees/postorder.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+struct node {
+	int data;
+	struct node * left;
+	struct node * right;
+};
+
+struct node* createNode(int data){
+	struct node * newNode=(struct node*)malloc(sizeof(struct node));
+	newNode->data=data;
+	newNode->left=NULL;
+	newNode->right=NULL;
+	return newNode;
+}
+
+
+void main(){
+	struct node * root = createNode(1);
+	root->left=createNode(2);
+	root->right=createNode(3);
+	root->left->left=createNode(4);
+	root->left->right=createNode(5);
+	
+	postOrder(root);
+}
+
+
+void postOrder(struct node * ptr) {
+	if(ptr != NULL){
+		postOrder(ptr->left);
+		postOrder(ptr->right);
+		printf("%d \n",ptr->data);
+	}
+}
